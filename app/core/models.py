2afbe319a15316a936b594e3dd35490ded0c94e4
amad3v <amad3v@gmail.com>
2020-04-03 09:28:27 +0100

    Updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    user = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
